37bd52e98604019e3a90e2f57fcf30e4788609ae
Dangphamk3 <phamhaidang250203@gmail.com>
2026-06-12 09:05:06 +0700

    Create auth-google.js

---
+++ api/auth-google.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+  const REDIRECT_URI = process.env.BASE_URL + '/api/callback-google';
+
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
+    `client_id=${CLIENT_ID}` +
+    `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
+    `&response_type=code` +
+    `&scope=${encodeURIComponent('https://www.googleapis.com/auth/calendar')}` +
+    `&access_type=offline` +
+    `&prompt=consent`;
+
+  res.redirect(authUrl);
+}
