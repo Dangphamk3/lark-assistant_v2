8637477fa53c01a3f7967f901ee2f0728982ead5
Dangphamk3 <phamhaidang250203@gmail.com>
2026-06-12 14:16:46 +0700

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,12 +1,10 @@
 export default function handler(req, res) {
   const APP_ID = process.env.LARK_APP_ID;
   const REDIRECT_URI = process.env.BASE_URL + '/api/callback';
-
   const authUrl = `https://open.larksuite.com/open-apis/authen/v1/authorize?` +
     `app_id=${APP_ID}` +
     `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
     `&response_type=code` +
-    `&scope=${encodeURIComponent('calendar:calendar im:message:readonly docx:document:readonly task:task:read task:task:write')}`;
-
+    `&scope=${encodeURIComponent('calendar:calendar im:message:readonly im:message im:message.send_as_user docx:document:readonly task:task:read task:task:write offline_access')}`;
   res.redirect(authUrl);
 }
