fa6083a3eac9bd5b6148cade99801b798808f0df
Dangphamk3 <phamhaidang250203@gmail.com>
2026-06-12 11:42:00 +0700

    Update auth-google.js

--- api/auth-google.js
+++ api/auth-google.js
@@ -1,14 +1,12 @@
 export default function handler(req, res) {
   const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
   const REDIRECT_URI = process.env.BASE_URL + '/api/callback-google';
-
   const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
     `client_id=${CLIENT_ID}` +
     `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
     `&response_type=code` +
-    `&scope=${encodeURIComponent('https://www.googleapis.com/auth/calendar')}` +
+    `&scope=${encodeURIComponent('https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/drive.readonly')}` +
     `&access_type=offline` +
     `&prompt=consent`;
-
   res.redirect(authUrl);
 }
