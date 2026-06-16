414ad330fcac7e525a3af2a6b37c58d27f61f640
Dangphamk3 <phamhaidang250203@gmail.com>
2026-06-16 12:09:44 +0700

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
     `app_id=${APP_ID}` +
     `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
     `&response_type=code` +
-    `&scope=${encodeURIComponent('calendar:calendar im:message:readonly im:message im:message.send_as_user docx:document:readonly task:task:read task:task:write contact:user.id:readonly offline_access')}` +
+    `&scope=${encodeURIComponent('calendar:calendar im:message:readonly im:message im:message.send_as_user im:chat:readonly docx:document:readonly task:task:read task:task:write contact:user.id:readonly offline_access')}` +
     `&state=${user}`;
   res.redirect(authUrl);
 }
